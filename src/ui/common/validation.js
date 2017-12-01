0e150820176c3c191bfe5e799aef03392f398ace
peng <yinpengroc@gmail.com>
2017-12-01 15:34:18 +1300

    test

---
+++ src/ui/common/validation.js
@@ -0,0 +1,31 @@
+
+import React, { Component } from 'react'
+import Recaptcha from 'react-recaptcha';
+const sitekey = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI';
+export default class validationComponent extends Component {
+    // specifying your onload callback function
+
+ callback = () => {
+    console.log('Done!!!!');
+  };
+  
+   verifyCallback = (response) => {
+    console.log(response);
+  };
+  
+   expiredCallback = () => {
+    console.log(`Recaptcha expired`);
+  };
+  render() {
+    return (
+      <div>
+          <Recaptcha
+          sitekey={sitekey}
+          verifyCallback={this.verifyCallback}
+          onloadCallback={this.callback}
+          expiredCallback={this.expiredCallback}
+        />
+      </div>
+    )
+  }
+}
